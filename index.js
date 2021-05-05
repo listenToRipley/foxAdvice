ac4b4740639b76961fc408f0d49ec00ca24147c7
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-05-05 17:37:43 -0500

    update to html 5 standard

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 `use strict`
 
-let fox = fetch(`https://randomfox.ca/floof/`) 
+window.onload = () => {
+  fox()
+  advice()
+  console.log('window load')
+}
+
+let fox = () => fetch(`https://randomfox.ca/floof/`) 
   .then(res => res.json())
   .then(data => data)
   .catch(err => console.log(err))
 
   console.log(fox)
 
-let advice = fetch(`https://api.adviceslip.com/`)
+let advice = () => fetch(`https://api.adviceslip.com/`)
   .then(res => res.json())
   .then(data => data)
   .then(err => console.log(err))
