2acafe35473863cbff60178116df94503228c44a
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-05-05 19:22:29 -0500

    found typo in html

--- index.js
+++ index.js
@@ -5,22 +5,27 @@ window.onload = () => {
   console.log('window load')
 }
 
-let fetchFox = () => fetch(`https://randomfox.ca/floof/`) 
-  .then(res => res.json())
-  .then(img => img)
-  .catch(err => console.log(err))
+let fetchFox = () => {
+  fetch(`https://randomfox.ca/floof/`) 
+    .then(res => res.json())
+    .then(img => img)
+    .catch(err => console.log(err))
+}
 
   console.log(fox)
 
-let fetchAdvice = () => fetch(`https://api.adviceslip.com/`)
-  .then(res => res.json())
-  .then(data => data)
-  .then(err => console.log(err))
+let fetchAdvice = () => {
+  fetch(`https://api.adviceslip.com/`)
+    .then(res => res.json())
+    .then(data => data)
+    .then(err => console.log(err))
+}
 
   console.log(fox)
 
 let update = () => {
-  fetchbox
+  fetchFox()
+  fetchAdvice()
 
   let foxImg = document.querySelectorAll('#foxImage')
   let adviceId = document.querySelectorAll('#advice')
